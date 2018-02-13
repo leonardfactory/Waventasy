ff017bd13c5f7cd0eb967b11ed571646cbbf0560
leonardfactory <leonardo.ascione@gmail.com>
2018-02-13 21:48:16 +0100

    Initial Commit

---
+++ Waventasy/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Waventasy
+//
+//  Created by Leonardo Ascione on 13/02/18.
+//  Copyright © 2018 Leonardo Ascione. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Waventasy/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Waventasy
+//
+//  Created by Leonardo Ascione on 13/02/18.
+//  Copyright © 2018 Leonardo Ascione. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
